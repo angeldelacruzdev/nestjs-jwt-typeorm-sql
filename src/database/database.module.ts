ae16e6c0a030815c355ec85ab1bd703b9371cd9e
Ángel De La Cruz <angeldelacruzdev@gmail.com>
2022-08-27 22:14:39 -0400

    Actualizado a la versión 9.0.11 de nestjs

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { User } from './../entities/user.entity';
-
+ 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: null })
+  name: string;
+
+  @Column({ default: null })
+  full_name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: null })
+  hashdRt: string | null;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
